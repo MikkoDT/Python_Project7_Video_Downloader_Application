45ec6ebe4c83c27c06504b06928f57a8d2f034a2
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-08-18 21:43:18 +0800

    Create video_downloaderapp.py

---
+++ code/video_downloaderapp.py
@@ -0,0 +1,42 @@
+from tkinter import *
+from tkinter import filedialog
+from pytube import YouTube
+from moviepy.editor import *
+
+def download():
+    video_path = url_entry.get()
+    file_path = path_label.cget("text")
+    mp4 =  YouTube(video_path).streams.get_highest_resolution().download()
+    video_clip = VideoFileClip(mp4)
+    video_clip.close()
+
+def get_path():
+    path = filedialog.askdirectory()
+    path_label.config(text=path)
+
+root = Tk()
+root.title('Video Downloader')
+canvas = Canvas(root,width=350,height=300)
+canvas.pack()
+
+#app label
+app_label = Label(root,text="Video downloader",fg='blue',font=('Arial',20))
+canvas.create_window(175,20,window=app_label)
+
+#entry to accept video URL
+url_label = Label(root,text="Enter video URL")
+url_entry = Entry(root)
+canvas.create_window(175,80,window=url_label)
+canvas.create_window(175,100,window=url_entry)
+
+#path to download videos
+path_label = Label(root,text="Selcet path to download")
+path_button = Button(root,text="Select",command=get_path)
+canvas.create_window(175,150,window=path_label)
+canvas.create_window(175,170,window=path_button)
+
+#download button
+download_button = Button(root,text='Download',command=download)
+canvas.create_window(175,250,window=download_button)
+
+root.mainloop()
